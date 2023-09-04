8b8d4c3b2c5afc7726112ad3c8708b8a33b16c2a
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2023-09-04 18:43:06 +0600

    Add files via upload

---
+++ JavaScript Fundamentals – Part 2/while loop.js
@@ -0,0 +1,10 @@
+
+// for (let i = 0; i <= 6; i++) {
+//     console.log(`Lefting weight🏋️‍♀️ repetation ${i}`);
+// }
+
+let i = 1;
+while (i <= 10) {
+    console.log(`I Love❤️ You😒 ${i}`);
+    i++;
+}
