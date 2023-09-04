5ef11b3946aca65471adfeab3bfc3e11b29ab74e
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2023-09-04 18:41:11 +0600

    Add files via upload

---
+++ JavaScript Fundamentals – Part 2/Loop into loop.js
@@ -0,0 +1,17 @@
+const alaminArray = [
+    'Alamin',
+    'Hossain',
+    2022 - 2000,
+    'Student',
+    ['Sohan', 'Sohag', 'Tarek']
+];
+
+for (let i = 4; i >= 0; i--) {
+    console.log(i, alaminArray[i]);
+}
+for (let i = 0; i <= 4; i++) {
+    console.log(`------Start Exercise ⛹️‍♂️ ${i}`);
+    for (let i = 0; i <= 6; i++) {
+        console.log(`Lefting weight🏋️‍♀️ repetation ${i}`);
+    }
+}
---
+++ JavaScript Fundamentals – Part 2/Loop.js
@@ -0,0 +1,10 @@
+// console.log('I love❤️ You!');
+// console.log('I love❤️ You!');
+// console.log('I love❤️ You!');
+// console.log('I love❤️ You!');
+// console.log('I love❤️ You!');
+// console.log('I love❤️ You!');
+//for loop keeps running while condition is true
+for (let rep = 1; rep <= 20; rep++) {
+    console.log(`I love❤️ You ${rep}!`);
+}
