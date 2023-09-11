ac26bb39e4234edab11b92555f45392f581688bc
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2023-09-11 16:51:46 +0600

    Add files via upload

---
+++ Data Structures, Modern Operators and Strings/Codding Challange 3.js
@@ -0,0 +1,26 @@
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "🟡 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🟡 Yellow card"],
+]);
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+gameEvents.delete(64);
+console.log(`An event happened, on average, every ${90 / gameEvents.size}`);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "First" : "Second";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
---
+++ Data Structures, Modern Operators and Strings/Coding Challange 2.js
@@ -0,0 +1,54 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+const gameScoreEntries = game.scored.entries();
+for (const [i, player] of gameScoreEntries) {
+  console.log(`${i + 1}: ${player}`);
+}
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
