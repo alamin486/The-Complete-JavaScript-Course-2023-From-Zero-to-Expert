92ad0009949e69d2dd6bfe6488e09b78beab7bac
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2023-09-18 18:38:38 +0600

    Add files via upload

---
+++ Asynchronous, Promises, AsyncAwait, and AJAX/AJAX call XMLHttpRequest/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    if (this.status === 200) {
+      const [data] = JSON.parse(this.responseText);
+      console.log(data);
+      const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 10000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${
+          data.languages[Object.keys(data.languages)[0]]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data.currencies[Object.keys(data.currencies)[0]].name
+        }</p>
+      </div>
+    </article>`;
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+      countriesContainer.style.opacity = 1;
+    } else {
+      console.error('Error loading country data');
+    }
+  });
+
+  request.addEventListener('error', function () {
+    console.error('Request failed');
+  });
+};
+getCountryData('bangladesh');
+getCountryData('india');
